d2807f87d3df3d0f6e785dee29601c8739310fbc
sl1123 <wyh@1123.com>
2021-01-04 21:21:14 +0800

    readme

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
+//导入组件
+import filmRouter from './routes/films'
+import centerRouter from './routes/center'
+import cinemasRouter from './routes/cinemas'
 const routes = [
- 
+
   // {
   //   path: '/about',
   //   name: 'About',
@@ -13,6 +16,10 @@ const routes = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
+  { path: "/", redirect: "/films" },
+  ...filmRouter,
+  centerRouter,
+  cinemasRouter
 ]
 
 const router = new VueRouter({
---
+++ src/router/routes/center.js
@@ -0,0 +1,6 @@
+import Center from '@/views/Center/Index.vue'
+var center = {
+  path: "/center", 
+  component: Center
+}
+export default center
